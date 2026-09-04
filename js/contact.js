9b0726dffb239016bca8c24ef4ac08074ba815ef
Pedro Manoel <pedro.borges@gruporiomais.com.br>
2026-09-04 16:23:00 -0300

    retirada e correção de codigos nas páginas para uma melhor compreensão

--- js/contact.js
+++ js/contact.js
@@ -1,85 +1,82 @@
-$(document).ready(function(){
-    
-    (function($) {
-        "use strict";
+$(document).ready(function () {
+  "use strict";
 
-    
-    jQuery.validator.addMethod('answercheck', function (value, element) {
-        return this.optional(element) || /^\bcat\b$/.test(value)
-    }, "type the correct answer -_-");
+  $("#contactForm").validate({
+    rules: {
+      name: {
+        required: true,
+        minlength: 2,
+      },
+      subject: {
+        required: true,
+        minlength: 4,
+      },
+      phone: {
+        required: true,
+        minlength: 10,
+      },
+      email: {
+        required: true,
+        email: true,
+      },
+      message: {
+        required: true,
+        minlength: 20,
+      },
+    },
 
-    // validate contactForm form
-    $(function() {
-        $('#contactForm').validate({
-            rules: {
-                name: {
-                    required: true,
-                    minlength: 2
-                },
-                subject: {
-                    required: true,
-                    minlength: 4
-                },
-                number: {
-                    required: true,
-                    minlength: 5
-                },
-                email: {
-                    required: true,
-                    email: true
-                },
-                message: {
-                    required: true,
-                    minlength: 20
-                }
-            },
-            messages: {
-                name: {
-                    required: "come on, you have a name, don't you?",
-                    minlength: "your name must consist of at least 2 characters"
-                },
-                subject: {
-                    required: "come on, you have a subject, don't you?",
-                    minlength: "your subject must consist of at least 4 characters"
-                },
-                number: {
-                    required: "come on, you have a number, don't you?",
-                    minlength: "your Number must consist of at least 5 characters"
-                },
-                email: {
-                    required: "no email, no message"
-                },
-                message: {
-                    required: "um...yea, you have to write something to send this form.",
-                    minlength: "thats all? really?"
-                }
-            },
-            submitHandler: function(form) {
-                $(form).ajaxSubmit({
-                    type:"POST",
-                    data: $(form).serialize(),
-                    url:"contact_process.php",
-                    success: function() {
-                        $('#contactForm :input').attr('disabled', 'disabled');
-                        $('#contactForm').fadeTo( "slow", 1, function() {
-                            $(this).find(':input').attr('disabled', 'disabled');
-                            $(this).find('label').css('cursor','default');
-                            $('#success').fadeIn()
-                            $('.modal').modal('hide');
-		                	$('#success').modal('show');
-                        })
-                    },
-                    error: function() {
-                        $('#contactForm').fadeTo( "slow", 1, function() {
-                            $('#error').fadeIn()
-                            $('.modal').modal('hide');
-		                	$('#error').modal('show');
-                        })
-                    }
-                })
-            }
-        })
-    })
-        
- })(jQuery)
-})
+    messages: {
+      name: {
+        required: "Por favor, informe seu nome.",
+        minlength: "Seu nome deve ter pelo menos 2 caracteres.",
+      },
+      subject: {
+        required: "Por favor, informe um assunto.",
+        minlength: "O assunto deve ter pelo menos 4 caracteres.",
+      },
+      phone: {
+        required: "Por favor, informe seu telefone.",
+        minlength: "Informe um telefone válido.",
+      },
+      email: {
+        required: "Por favor, informe seu e-mail.",
+        email: "Informe um e-mail válido.",
+      },
+      message: {
+        required: "Por favor, escreva uma mensagem.",
+        minlength: "Sua mensagem deve ter pelo menos 20 caracteres.",
+      },
+    },
+
+    submitHandler: function (form) {
+      $(form).ajaxSubmit({
+        type: "POST",
+        data: $(form).serialize(),
+        url: "contact_process.php",
+
+        success: function () {
+          $("#contactForm :input").attr("disabled", "disabled");
+
+          $("#contactForm").fadeTo("slow", 1, function () {
+            $(this).find(":input").attr("disabled", "disabled");
+            $(this).find("label").css("cursor", "default");
+
+            $("#success").fadeIn();
+
+            $(".modal").modal("hide");
+            $("#success").modal("show");
+          });
+        },
+
+        error: function () {
+          $("#contactForm").fadeTo("slow", 1, function () {
+            $("#error").fadeIn();
+
+            $(".modal").modal("hide");
+            $("#error").modal("show");
+          });
+        },
+      });
+    },
+  });
+});
